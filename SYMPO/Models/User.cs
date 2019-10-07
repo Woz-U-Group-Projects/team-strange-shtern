1cf7b9fd92a033195af1e4dea9d77636368be4e9
Donna M Wilson <48892524+donnammartin81@users.noreply.github.com>
2019-10-07 16:52:20 -0500

    Add files via upload

---
+++ SYMPO/Models/User.cs
@@ -0,0 +1,13 @@
+namespace Sympo {
+    public class User {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public int? LastName { get; set; }
+        public int Email { get; set; }
+        public int? Address { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Apartment { get; set; }
+       
+
+    }
+}
